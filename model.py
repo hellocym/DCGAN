d3b3f0aa4e20aea86595eaa4a6e80b4bdaf2fadc
hellocym <13681921379@163.com>
2023-06-24 10:18:31 +0000

    init

---
+++ model.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, channels_img, features_d=64):
+        """
+        channels_img: number of channels in the images
+        features_d: number of features in the first layer of the discriminator
+        """
+        super(Discriminator, self).__init__()
+        self.D = nn.Sequential(
+            # Input: N x channels_img x 64 x 64
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            # Output: N x features_d x 32 x 32
+            self._block(features_d, features_d*2, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_d*2 x 16 x 16
+            self._block(features_d*2, features_d*4, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_d*4 x 8 x 8
+            self._block(features_d*4, features_d*8, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_d*8 x 4 x 4
+            nn.Conv2d(features_d*8, 1, kernel_size=4, stride=2, padding=0),
+            # Output: N x 1 x 1 x 1
+            nn.Sigmoid(),
+        )
+    
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+        
+    
+    def forward(self, x):
+        return self.D(x)
+    
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, features_g=64):
+        """
+        z_dim: dimension of the noise vector
+        channels_img: number of channels in the images
+        features_g: number of features in the first layer of the generator
+        """
+        super(Generator, self).__init__()
+        self.G = nn.Sequential(
+            # Input: N x z_dim x 1 x 1
+            self._block(z_dim, features_g*16, kernel_size=4, stride=1, padding=0), 
+            # Output: N x features_g*16 x 4 x 4
+            self._block(features_g*16, features_g*8, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_g*8 x 8 x 8
+            self._block(features_g*8, features_g*4, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_g*4 x 16 x 16
+            self._block(features_g*4, features_g*2, kernel_size=4, stride=2, padding=1),
+            # Output: N x features_g*2 x 32 x 32
+            nn.ConvTranspose2d(features_g*2, channels_img, kernel_size=4, stride=2, padding=1),
+            # Output: N x channels_img x 64 x 64
+            nn.Tanh(), 
+            # Output: N x channels_img x 64 x 64, range [-1, 1]
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.G(x)
+    
+
+def initialize_weights(model):
+    """
+    Initialize weights of the model with N(0, 0.02)
+    """
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    
+def test():
+    N, in_channels, H, W = 8, 3, 64, 64
+    z_dim = 100
+    x = torch.randn((N, in_channels, H, W))
+    D = Discriminator(in_channels, 8)
+    initialize_weights(D)
+    assert D(x).shape == (N, 1, 1, 1), "Discriminator test failed"
+    G = Generator(z_dim, in_channels, 8)
+    initialize_weights(G)
+    z = torch.randn((N, z_dim, 1, 1))
+    assert G(z).shape == (N, in_channels, H, W), "Generator test failed"
+    print("All tests passed")
---
+++ train.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model import Discriminator, Generator, initialize_weights
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Running on {device}')
+
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+Z_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_D = 64
+FEATURES_G = 64
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]),
+    ]
+)
+
+dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms, download=True)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+G = Generator(Z_DIM, CHANNELS_IMG, FEATURES_G).to(device)
+D = Discriminator(CHANNELS_IMG, FEATURES_D).to(device)
+initialize_weights(G)
+initialize_weights(D)
+
+optim_G = optim.Adam(G.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+optim_D = optim.Adam(D.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
+writer_real = SummaryWriter(f"logs/real")
+writer_fake = SummaryWriter(f"logs/fake")
+step = 0
+
+G.train()
+D.train()
+
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (real, _) in enumerate(dataloader):
+        real = real.to(device)
+        noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+        fake = G(noise)
+
+        # Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+        D_real = D(real).reshape(-1)
+        loss_D_real = criterion(D_real, torch.ones_like(D_real))
+        D_fake = D(fake).reshape(-1)
+        loss_D_fake = criterion(D_fake, torch.zeros_like(D_fake))
+        loss_D = (loss_D_real + loss_D_fake) / 2
+
+        D.zero_grad()
+        loss_D.backward(retain_graph=True)
+        optim_D.step()
+
+        # Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+        output = D(fake).reshape(-1)
+        loss_G = criterion(output, torch.ones_like(output))
+        G.zero_grad()
+        loss_G.backward()
+        optim_G.step()
+
+        if batch_idx % 100 == 0:
+            print(
+                f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(dataloader)} \
+                  Loss D: {loss_D:.4f}, loss G: {loss_G:.4f}"
+            )
+
+            with torch.no_grad():
+                fake = G(fixed_noise)
+                # take out (up to) 32 examples
+                img_grid_real = torchvision.utils.make_grid(
+                    real[:32], normalize=True
+                )
+                img_grid_fake = torchvision.utils.make_grid(
+                    fake[:32], normalize=True
+                )
+
+                writer_real.add_image(
+                    "Mnist Real Images", img_grid_real, global_step=step
+                )
+                writer_fake.add_image(
+                    "Mnist Fake Images", img_grid_fake, global_step=step
+                )
+            step += 1
